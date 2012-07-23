fb743703a3a2f07e6d63fd24654c740abb5bc9bd
System Administrator <root@Daves-iPad.(none)>
2012-07-23 09:49:19 +0100

    Working on the request object.

--- library/Ubui/Http/index.js
+++ library/Ubui/Http/index.js
@@ -1,18 +1,24 @@
 function Http () {
 	var
 		http = require('http'),
-		server = undefined;
+		server = undefined,
+		_request, _response;
 	
 	/*This function is simply a wrapper for
 	our other classes Http_Request and Http_Response*/
 	server = http.createServer(function (request, response) {
 		//Fire the request and response classes
-		new Ubuif.Http_Request(request);
-		new Ubuif.Http_Response(response);
+		_request = new Ubuif.Http_Request(request);
+		_response = new Ubuif.Http_Response(response);
 	});
 	
 	//Listen to the port specified in the config file
 	server.listen(Ubuif.private.config.server.port);
+	
+	//Methods for getting request information
+	this.getRequest = function () {
+		return _request;
+	};
 	
 	return this;
 };
--- library/Ubui/Http_Request/index.js
+++ library/Ubui/Http_Request/index.js
@@ -1,5 +1,26 @@
 function Http_Request (request) {
 	
+	var
+		_server = request.headers.host,
+		_request_uri = request.url;
+	
+	this.getUrlParts = function () {
+		return _request_uri.split('/').slice(1);
+	};
+	
+	this.isPost = function () {
+		return (request.method === 'POST');
+	};
+	
+	this.getController = function () {
+		return this.getUrlParts()[0];
+	};
+	
+	this.getAction = function () {
+		return this.getUrlParts()[1];
+	};
+	
+	console.log(this.getUrlParts());
 }
 
 Ubuif.Http_Request = Http_Request;
