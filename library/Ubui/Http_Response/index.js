a8f5d311fc58beeeac5d1f361adc98619e486ba2
System Administrator <root@Daves-iPad.(none)>
2012-08-06 09:26:52 +0100

    Adding functions to deliver the layouts to the browser

--- library/Ubui/Http_Response/index.js
+++ library/Ubui/Http_Response/index.js
@@ -2,14 +2,30 @@ function Http_Response (response) {
 
 	var
 		_response = response,
-		_controller, _action;
+		_controller,
+		_action,
+		_body;
+		
+	this.rTemplate = /#*#/;
 	
-	this.getController = function () {
-		console.log(this.controller);
+	this.header = function (header, type) {
+		header = Number(header) || 200;
+		type = type.toString() || 'text/plain';
+		
+		_response.writeHead(header, {
+			"Content-Type": type
+		});
+		
+		return this;
 	};
 	
-	this.getAction = function () {
+	this.setBody = function (body) {
+		_body = body;
+		return this;
+	};
 	
+	this.getBody = function () {
+		return _body;
 	};
 	
 	this.FourOhFour = function () {
@@ -23,7 +39,37 @@ function Http_Response (response) {
 	this.setHeader = function (header, options) {
 		_response.writeHead(header, options);
 	};
-}
+	
+	this.render = function (view) {
+		var vari;
+		// Check we have the view file
+		Ubuif.FileSystem.isRequirable(view, function (is) {
+			if (is) {
+				for (vari in Ubuif.View) {
+					
+				};
+			} else {
+				throw Error('View script "' + action + '" cannot be found');
+			}
+		});
+		
+		return this;
+	};
+	
+	this.deliver = function (controller, action) {
+		var viewScript = 'Ubuif/../../application/views/' + 
+				controller + '/' + action + '.js';
+		// Open the instance of the requested controller
+		// with the scope of Ubuif.Http
+		_response.controller = new (require(controller))();
+		_response.controller[action + 'Action'].call(Ubuif.Http);
+		
+		this.render(viewScript);
+		
+		// End the reponse
+		_response.end();
+	};
+};
 
 Ubuif.Http_Response = Http_Response;
 module.exports = Http_Response;
