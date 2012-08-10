1d6e95191fdce6a77717a3677032d7cee2480f7d
System Administrator <root@Daves-iPad.(none)>
2012-08-10 09:36:40 +0100

    Working on getting html from the template to the browser.

--- library/Ubuif/View/ubuivooAdapter.js
+++ library/Ubuif/View/ubuivooAdapter.js
@@ -1,9 +1,11 @@
 function UbuivooAdapter () {
 	
-	var _core = Ubuif.Ubuivoo;
+	var _core = new Ubuif.Ubuivoo();
 	// Literally just an interface to the actual
 	// module we have installed on the framework
 	this.render = _core.render;
+	
+	this.compile = _core.compile;
 	
 	return this;
 };
