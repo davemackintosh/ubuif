6739e9fabe99d23e7d5ad25d81554301ebb837c8
System Administrator <root@Daves-iPad.(none)>
2012-09-19 17:51:55 +0100

    Added view support for loops (objects/arrays), in view variable setting and examples in the views

--- application/controllers/Index.js
+++ application/controllers/Index.js
@@ -5,8 +5,21 @@ function IndexController () {
 	};
 	
 	this.indexAction = function () {
+		this.getFrontEnd().setLayout('layout.html');
 		Ubuif.View.title = 'UBUIF FRAMEWORK TEST';
 		Ubuif.View.userName = 'Dave';
+		Ubuif.View.testObject = {
+			"hello": "world",
+			"goodbye": "world"
+		};
+		
+		Ubuif.View.testArray = [1,2,3,4,5,6,7,8,9];
+		Ubuif.View.multidimensional = [];
+		Ubuif.View.multidimensional['array'] = [1,2,3,4,5];
+		Ubuif.View.multidimensional['object'] = {
+			'hello': 'world',
+			'world': 'hello'
+		};
 	};
 	
 	this.testAction = function () {
